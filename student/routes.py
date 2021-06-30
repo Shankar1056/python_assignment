bb4634cc913cd3d945096730e30a863f0ab0f8af
Shankar Kumar <dayashankargupta86@gmail.com>
2021-06-30 15:32:55 +0530

    signup, login, update user, and delete users, display user/usrs -> Working code

--- student/routes.py
+++ student/routes.py
@@ -1,7 +1,10 @@
+from werkzeug.utils import redirect
+
 from student import app, Students, db
 from flask import request, render_template, Blueprint, flash
 
 usercontrol = Blueprint("usercontrol", __name__)
+
 
 # Signup
 @app.route("/signup", methods=['GET', 'POST'])
@@ -15,20 +18,21 @@ def signup():
     userEmail = data['email']
     userPassword = data['password']
 
-    # users = Students.query.filter_by(email=userEmail).first()
-    # if users:
-    #  flash('Email address already exists',' error')
+    users = Students.query.filter_by(email=userEmail).first()
+    if users:
+        flash('Email address already exists', ' error')
 
-    #  return render_template("signup.html")
+        return render_template("signup.html")
 
     new_user = Students(username=userName, email=userEmail, password=userPassword)
     db.session.add(new_user)
     db.session.commit()
 
-    return render_template("login.html")
+    return redirect("/")
+
 
 # Login
-@app.route("/login", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def login():
     # logic
     if request.method == 'GET':
@@ -43,24 +47,35 @@ def login():
     # flash('Please check Email and try again.')
     # return render_template("login.html")
 
+    userDetails = Students.query.filter_by(email=userEmail).first()  # --- get user details from the DB
+    print(userDetails.username)
+    return render_template("/user_details.html", users=userDetails)
+
+
+# to delet the user based on ID
+@app.route("/display")
+def displayAllUsers():
+    # logic
     allusers = Students.query.all()  # --- get all the values from the DB
+    # print(allusers.email)
     return render_template("all_students.html", users=allusers)
 
 
 # to delet the user based on ID
-@app.route("/delete/<id>")
-def delete(id):
+@app.route("/delete/")
+def delete():
     # logic
-    print(id)
-    db.session.query(Students).filter(Students.id == id).delete()
+
+    db.session.query(Students).delete()
     db.session.commit()
-    print("success")
+    return redirect("/signup")
 
 
 # to update the user based on ID
-@app.route("/update/<id>")
-def update(id):
+@app.route("/update")
+def update():
     # logic
-    admin = Students.query.filter_by(id=id).first()
+    admin = Students.query.filter_by(email="shankar@gmail.com").first()
     admin.username = 'Daya Shankar'
     db.session.commit()
+    return redirect("/display")
